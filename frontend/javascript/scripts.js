e15d59e6f05e851b23e11666b1999b92349d8522
Nivesh <105005447+Nivesh30@users.noreply.github.com>
2023-07-26 07:04:54 +0200

    Update scripts.js

--- frontend/javascript/scripts.js
+++ frontend/javascript/scripts.js
@@ -1,12 +1,12 @@
-// Function to fetch data from the API
+// JavaScript function to fetch data from API
 function fetchData() {
     const apiUrl = 'http://localhost:3000/api/page'; 
 
-    // Using the fetch API to make a GET request
+    // using the fetch API to GET data
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Call a function to display the data on the page
+            // call the function to display the data on the page
             displayData(data);
         })
         .catch(error => {
@@ -14,14 +14,14 @@ function fetchData() {
         });
 }
 
-// Function to display the fetched data on the HTML page
+// function to display the fetched data on the HTML page
 function displayData(data) {
     const dataContainer = document.getElementById('data-container');
 
-    // Clear the existing content in the container
+    // clear the existing content in the container
     dataContainer.innerHTML = '';
 
-    // Loop through the data and create HTML elements to display it
+    // loop through the data and create HTML elements to display it
     data.forEach(item => {
         const priceElement = document.createElement('p');
         priceElement.textContent = item.price;
@@ -35,5 +35,5 @@ function displayData(data) {
     });
 }
 
-// Call the fetchData function when the page loads
+// call the fetchData function when the page loads
 window.onload = fetchData;
